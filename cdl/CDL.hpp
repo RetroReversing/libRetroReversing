65bdfd14c78bfc40a72058320250c0fa47227682
A <>
2021-01-03 20:19:11 +0000

    boost didn't help with the windows build

--- cdl/CDL.hpp
+++ cdl/CDL.hpp
@@ -13,15 +13,15 @@
 #ifndef __has_include
   static_assert(false, "__has_include not supported");
 #else
-#  if __has_include(<boost/filesystem.hpp>)
-#    include <boost/filesystem.hpp>
-     namespace fs = boost::filesystem;
-#  elif __has_include(<experimental/filesystem>)
+#  if __has_include(<experimental/filesystem>)
 #    include <experimental/filesystem>
      namespace fs = std::experimental::filesystem;
 #  elif __has_include(<filesystem>)
 #    include <filesystem>
      namespace fs = std::fs::filesystem;
+#  elif __has_include(<boost/filesystem.hpp>)
+#    include <boost/filesystem.hpp>
+     namespace fs = boost::filesystem;
 #  endif
 #endif
 
